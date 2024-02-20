7cefa2bf1da822e45f2ffe73b53d01a5005a6646
Alero Awani <aleroawani18@gmail.com>
2024-02-21 00:55:56 +0100

    Random data generation

---
+++ sample/generator.go
@@ -0,0 +1,59 @@
+package sample
+
+import pb "github.com/aleroawani/grpc/pb/proto"
+
+// NewKeyboard returns a new sample keyboard
+func NewKeyboard() *pb.Keyboard {
+	keyboard := &pb.Keyboard{
+		Layout: randomKeyboardLayout(),
+		Backlit: randomBool(),
+
+
+	}
+	return keyboard
+
+}
+
+// NewCPU returns a new sample CPU 
+
+func NewCPU() *pb.CPU {
+	brand := randomCPUBrand()
+	name := randomCPUName(brand)
+
+	numberCores := randomInt(2,8)
+	numberThreads := randomInt(numberCores, 12)
+
+	minGhz := randomFloat64(2.0, 3.5)
+	maxGhz := randomFloat64(minGhz, 5.0)
+
+
+
+
+	cpu := &pb.CPU{
+		Brand: brand,
+		Name: name,
+		NumberCores: uint32(numberCores),
+		NumberThreads: uint32(numberThreads),
+		MinGhz: minGhz,
+		MaxGhz: maxGhz,
+
+	}
+
+	return cpu
+}
+
+// NewGPU returns a new sample GPU
+func NewGPU() *pb.GPU {
+	brand := randomGPUBrand()
+	name := randomGPUName(brand)
+
+	minGhz := randomFloat64(1.0, 1.5)
+	mazGhz := randomFloat64(minGhz, 2.0)
+	
+	gpu := &pb.GPU{
+		Brand: brand,
+		Name: name,
+	}
+
+	return gpu 
+}
---
+++ sample/random.go
@@ -0,0 +1,60 @@
+package sample
+
+import (
+	"math/rand"
+
+	pb "github.com/aleroawani/grpc/pb/proto"
+)
+
+
+func randomKeyboardLayout() pb.Keyboard_Layout {
+	switch rand.Intn(3) {
+	case 1:
+		return pb.Keyboard_QWERTY
+	case 2:
+		return pb.Keyboard_QWERTZ
+	default:
+		return pb.Keyboard_AZERTY
+	}
+
+}
+
+func randomBool() bool {
+	return rand.Intn(2) == 1
+}
+
+func randomCPUBrand() string {
+	return randomStringFromSet("Intel", "AMD")
+}
+
+func randomCPUName(brand string) string {
+	if brand == "Intel" {
+		return randomStringFromSet(
+			"Xeon E-2286M",
+			"Core i9-9980HK",
+			"Core i7-9750H",
+			"Core i5=9400F",
+		)
+	}
+	return randomStringFromSet(
+		"Ryzen 7 PRO 2700U",
+		"Ryzen 5 PRO 3500U",
+		"Ryzen 3 PRO 3200GE",
+	)
+}
+
+func randomStringFromSet(a ...string) string {
+	n := len(a)
+	if n == 0 {
+		return ""
+	}
+	return a[rand.Intn(n)]
+}
+
+func randomInt(min int, max int) int {
+	return min + rand.Intn(max-min+1)
+}
+
+func randomFLoat64(min float64, max float64) float64 {
+	return min + rand.Float64()*(max-min)
+}
